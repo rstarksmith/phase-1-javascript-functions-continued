6afafa55a17c0295f3b529da6b676666982b5d1a
Rachel <rstark.smith@gmail.com>
2022-03-18 13:04:39 -0500

    commit tree 2d59839812b6c69b4a1e6194fbcf3a2d40b58e52

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// function saturdayFun(activity) {
+//     if(activity = 'null'){
+//     return `This Saturday, I want to roller-skate!`;
+//     } else {
+//         return `This Saturday, I want to ${activity}!`;
+//     }
+// }
+
+
+function saturdayFun(activity = 'roller-skate') {
+        return `This Saturday, I want to ${activity}!`;
+    }
+
+
+const mondayWork = function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = 'special') {
+        return (`You are ${flair}${adjective}${flair}!`);
+    }
+}
